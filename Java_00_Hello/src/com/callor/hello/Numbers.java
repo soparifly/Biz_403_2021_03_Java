9d806defa576c8c1c7aa0d423ea6720ad4a04bcd
soparifly <soparifly@naver.com>
2021-03-17 09:18:47 +0900

    moning report

---
+++ Java_00_Hello/src/com/callor/hello/Numbers.java
@@ -0,0 +1,21 @@
+package com.callor.hello;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		
+		System.out.print("33 + 2 = ");
+		System.out.println(33 + 2);
+		System.out.print("33 - 2 = ");
+		System.out.println(33 - 2);
+		System.out.print("33 * 2 = ");
+		System.out.println(33 * 2);
+		System.out.print("33 / 2 = ");
+		System.out.println(33 / 2);
+		System.out.print("33 % 2 = ");
+		System.out.println(33 % 2);
+		
+}
+	}
+
+
