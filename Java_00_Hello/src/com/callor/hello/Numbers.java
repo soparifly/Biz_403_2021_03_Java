9842141169e0831872e6c87f2c84b1d6e29dbc56
soparifly <soparifly@naver.com>
2021-03-17 10:01:17 +0900

    3/17 아침레포트 풀이

--- Java_00_Hello/src/com/callor/hello/Numbers.java
+++ Java_00_Hello/src/com/callor/hello/Numbers.java
@@ -1,8 +1,9 @@
 package com.callor.hello;
-
+ // com.callor.hello <<베이스 패키지
 public class Numbers {
 
 	public static void main(String[] args) {
+		System.out.println("===========================");
 		
 		System.out.print("33 + 2 = ");
 		System.out.println(33 + 2);
@@ -14,7 +15,8 @@ public class Numbers {
 		System.out.println(33 / 2);
 		System.out.print("33 % 2 = ");
 		System.out.println(33 % 2);
-		
+
+		System.out.println("===========================");
 }
 	}
 
---
+++ Java_00_Hello/src/com/callor/hello/Numbers01.java
@@ -0,0 +1,10 @@
+package com.callor.hello;
+// 소스 확인하는법 : 내컴퓨터 -> 로컬디스크 > javawors -> 프로젝트폴더 -> src -> 베이스 프로젝트의 각단어마다 폴더이름의 경로가 있음.
+// class 파일은 위 경로와 동일하나 src폴더가아닌 bin 폴더 내부의 각단마다 폴더이름의 경로를 타고들어가면됨
+//bin 폴더가 삭제될경우 상단 메뉴 project 메뉴에서 clean을 실행하면 재생성이가능함
+public class Numbers01 {
+	public static void main(String []args) {
+	 System.out.println("============================");
+	 System.out.println("33 + 2 = " + (33+2));
+ }
+}
