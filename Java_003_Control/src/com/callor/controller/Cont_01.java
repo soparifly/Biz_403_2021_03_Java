0bcbc0607cbfffb915ac10235d1db4076ab46d2a
soparifly <soparifly@naver.com>
2021-03-19 09:17:43 +0900

    아침과제

---
+++ Java_003_Control/src/com/callor/controller/Cont_01.java
@@ -0,0 +1,19 @@
+package com.callor.controller;
+
+public class Cont_01 {
+
+	public static void main(String[] args) {
+
+		int intNum = 0;
+
+		System.out.println("==========================================");
+		for (; intNum++ < 10;) {
+			System.out.printf("%d", intNum);
+			System.out.println(".우리나라 대한민국");
+		}
+		System.out.println("==========================================");
+
+	}
+}
+
+//ctrl + shift + F < 줄정렬
